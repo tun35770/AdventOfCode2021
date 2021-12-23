8561a447b8f10ea547157b3362ba4632128c551c
tun35770 <tun35770@temple.edu>
2021-12-23 00:40:56 -0500

    Day 9 Complete

--- Day 9 - Smoke Basin/index.js
+++ Day 9 - Smoke Basin/index.js
@@ -21,11 +21,16 @@ async function main(){
     await readInput();
     let heightMap = parseInput();
     
-    findLowestPoints(heightMap);
+    //findLowestPoints(heightMap);  //Part 1
+
+    findBasins(heightMap)           //Part 2
 }
 
+var visited = new Map();
 main();
 
+//finds the lowest points of the heightMap and calculates the
+//total risk level (risk = height + 1 for each low point)
 function findLowestPoints(heightMap){
     let totalRiskLevel = 0;
 
@@ -57,6 +62,64 @@ function findLowestPoints(heightMap){
     }
 
     console.log(`Total Risk level: ${totalRiskLevel}`);
+}
+
+
+//For some reason, second and third are WRONG
+//Correct numbers were 102,96,92 which i found by sorting
+//the sizes array and checking manually
+//IDK why basic comparison seems to not want to work here
+
+//Finds sizes of largest 3 basins in the heightMap
+function findBasins(heightMap){
+    let first = 0, second = 0, third = 0;
+    let sizes = [];
+
+    for(let i = 0; i < heightMap.length; i++){
+        
+        for(let j = 0; j < heightMap[0].length; j++){
+           
+            if(heightMap[i][j] != 9)  {  
+                let size = checkForBasin(heightMap, i, j);
+                sizes.push(size);
+
+                if(size > first)
+                    first = size;
+                else if(size > second)
+                    second = size;
+                else if(size > third)
+                    third = size;
+            }
+        }
+    }
+
+    console.log(`Largest Three Basins: 1) ${first} 2) ${second} 3) ${third}`);
+    console.log(`Product: ${first*second*third}`);
+}
+
+function checkForBasin(heightMap, row, col){
+    
+    if(heightMap[row][col] == 9)
+        return 0;
+
+    if(visited.get( heightMap.length * row + col))
+        return 0;
+
+    //mark as visited
+    visited.set(heightMap.length * row + col, 1);
+
+
+    let size = 1;
+    if(row > 0)   
+        size += checkForBasin(heightMap, row - 1, col);
+    if(row < heightMap.length - 1)    
+        size += checkForBasin(heightMap, row + 1, col);
+    if(col > 0)   
+        size += checkForBasin(heightMap, row, col - 1);
+    if(col < heightMap[0].length - 1)  
+        size += checkForBasin(heightMap, row, col + 1);
+
+    return size;
 }
 
 function parseInput(){
