5eed1752e7157c04036a42e9a9a9e79b6eeb8bde
tun35770 <tun35770@temple.edu>
2021-12-24 17:22:29 -0500

    Day 10 Complete

--- Day 10 - Syntax Scoring/index.js
+++ Day 10 - Syntax Scoring/index.js
@@ -18,11 +18,16 @@ async function readInput(){
 
 async function main(){
     await readInput();
-    parseStack(input);
+    //parseStack(input);  //Part 1
+    incompleteScore(input);
 }
 
 main();
 
+//Determine corrupted score of all lines
+//If an opening (, {, [, or < is closed with the wrong bracket,
+//score is increased based on value given by problem
+//If so, increase score and move to next line
 function parseStack(input){
 
     let score = 0;
@@ -58,4 +63,86 @@ function parseStack(input){
     })
 
     console.log(`Corrupted Score: ${score}`);
+}
+
+//Find median score of completion scores
+//For only non-corrupt lines, find brackets required to complete it
+//Score each line based on point values in problem
+function incompleteScore(input){
+    let scores = [];
+    let filteredInput = removeCorruptedLines(input);
+
+    filteredInput.forEach(line => {
+        let stack = [];
+        let completion = [];
+
+        //build stack
+        for(let i = 0; i < line.length; i++){
+            let c = line.charAt(i);
+            if(c == '{' || c == '(' || c == '[' || c == '<'){
+                stack.push(c);
+            }
+            else    stack.pop();
+        }
+
+        let score = 0;
+        while(stack.length > 0){
+            let c = stack.pop()
+            score *= 5;
+            if(c == '('){
+                score++;
+                completion.push(')');
+            }
+            else if(c == '['){
+                score += 2;
+                completion.push(']');
+            }
+            else if(c == '{'){
+                score += 3;    
+                completion.push('}');
+            }
+            else if(c == '<'){
+                score += 4;    
+                completion.push('>');
+            }
+        }
+
+        scores.push(score);
+    })
+
+    //sort scores, get median
+    scores.sort(function(a, b) {
+        return a - b;
+      });
+      
+    console.log(scores);
+    console.log(`Median Score: ${scores[(scores.length-1)/2]}`);
+}
+
+function removeCorruptedLines(input){
+    for(let i = 0; i < input.length; i++){
+        let line = input[i];
+        let stack = [];
+
+        for(let j = 0; j < line.length; j++){
+            let c = line.charAt(j);
+            if(c == '{' || c == '(' || c == '[' || c == '<'){
+                stack.push(c);
+            }
+            else{
+                let s = stack.pop();
+                if( (c == ')' && s != '(') ||
+                    (c == ']' && s != '[') ||
+                    (c == '}' && s != '{') ||
+                    (c == '>' && s != '<')){
+                        console.log(`Removed: ${s}${c}`);
+                        input.splice(i, 1);
+                        i--;
+                        break;
+                }
+            }
+        }
+    }
+
+    return input;
 }
