c00a6a0d7b2f5ceafd78c1db9b9a42d852e9d5cf
tun35770 <tun35770@temple.edu>
2021-12-24 22:28:29 -0500

    Day 11 Part 1 Complete

---
+++ Day 11 - Dumbo Octopus/index.js
@@ -0,0 +1,105 @@
+const fs = require('fs');
+const readline = require('readline');
+
+var input = [];
+
+async function readInput(){
+    
+    const rl = readline.createInterface({
+        input: fs.createReadStream('./Day 11 - Dumbo Octopus/day11.txt'),
+        output: process.stdout,
+        console: false
+    })
+    
+    for await(const line of rl){
+        input.push(line);
+    }
+}
+
+async function main(){
+    await readInput();
+    findTotalFlashes(input);
+}
+
+let totalFlashes = 0;
+main();
+
+function findTotalFlashes(input){
+    let map = [];
+    let flashed = new Map();
+
+    input.forEach(element => {
+        map.push(element.split(''));
+    });
+
+    for(let i = 0; i < 100; i++){
+        for(let j = 0; j < map.length; j++){
+            for(let k = 0; k < map[0].length; k++){
+                map[j][k]++;
+            }
+        }
+
+        //check for flashes
+        for(let j = 0; j < map.length; j++){
+            for(let k = 0; k < map[0].length; k++){
+                checkFlash(map, j, k, flashed);
+            }
+        }
+        
+        //set all falshed octopus energy to 0
+        for(let j = 0; j < map.length; j++){
+            for(let k = 0; k < map[0].length; k++){
+                if(flashed.get(map.length * j + k))
+                    map[j][k] = 0;
+            }
+        }
+        flashed.clear();
+    }
+
+    console.log(`Total Flashes: ${totalFlashes}`);
+}
+
+function checkFlash(map, i, j, flashed){
+    if(i < 0 || j < 0 || i > map.length-1 || j > map[0].length-1)
+        return;
+
+    //flash!
+    if(map[i][j] > 9){
+        //if this octopus hasn't already flashed this step
+        if(!flashed.get(map.length * i + j)){
+
+            flashed.set(map.length * i + j, 1);
+            totalFlashes++;
+
+            if(i > 0 && j > 0)
+                map[i-1][j-1]++;
+            if(i > 0)
+                map[i-1][j]++;
+            if(i > 0 && j < map[0].length-1)
+                map[i-1][j+1]++;
+            if(j < map[0].length-1)
+                map[i][j+1]++; 
+            if(i < map.length-1 && j < map[0].length-1)
+                map[i+1][j+1]++;
+            if(i < map.length-1)
+                map[i+1][j]++;
+            if(i < map.length-1 && j > 0)
+                map[i+1][j-1]++;
+            if(j > 0)
+                map[i][j-1]++;
+                
+            //clockwise from top-left
+            checkFlash(map, i-1, j-1, flashed);
+            checkFlash(map, i-1, j, flashed);
+            checkFlash(map, i-1, j+1, flashed);
+            checkFlash(map, i, j+1, flashed);
+            checkFlash(map, i+1, j+1, flashed);
+            checkFlash(map, i+1, j, flashed);
+            checkFlash(map, i+1, j-1, flashed);
+            checkFlash(map, i, j-1, flashed);
+
+        }   
+    }
+
+    return;
+}
