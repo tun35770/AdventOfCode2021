8e0aacde463ece130528dbd4b85d15ce6a9c2bef
tun35770 <tun35770@temple.edu>
2021-12-26 02:07:18 -0500

    Day 12 Complete

--- Day 12 - Passage Pathing/index.js
+++ Day 12 - Passage Pathing/index.js
@@ -24,6 +24,13 @@ async function main(){
 
 main();
 
+
+//Depth First Search through a graph using recursion
+//Big caves can be revisited any number of times
+//Small caves only once (except in Part 2, where a single
+//small cave may be visited twice)
+//Builds adjacency list map then calls DFS to find total # of paths
+//from start to end
 function findPaths(input){
     let nodeMap = new Map();
 
@@ -60,34 +67,67 @@ function findPaths(input){
             nodeMap.get(inputTuple[1]).edges.push(inputTuple[0]);
         }
     }
-    
-    let totalPaths = DFS(nodeMap.get('start'), nodeMap, new Map());
 
-    console.log(`Total Paths: ${totalPaths}`);
+    let totalPaths = 0;
 
+    //Part 1
+    totalPaths += DFS(nodeMap.get('start'), nodeMap, new Map(), null);
+    console.log(`Total Paths with no revisits: ${totalPaths}`);
+
+    nodeMap.forEach((value, key) => {
+        console.log(`${key}, ${value.edges}`);  
+    })
+
+    //Part 2
+    nodeMap.forEach((value, key) => {
+        if(!value.isBig && key != 'start' && key != 'end'){
+            console.log(`${key}`)
+            totalPaths += DFS(nodeMap.get('start'), nodeMap, new Map(), key) - 5756;    //dont recount paths
+        }                                                                           //without any revisits
+    })
+
+    console.log(`Total Paths with revisits: ${totalPaths}`);
 }
 
-const DFS = (start, nodeMap, haveVisited) => {
+const DFS = (start, nodeMap, haveVisited, revisit) => {
 
-    //dont revisit small caves
-    if(!start.isBig && haveVisited.has(start.name))
-        return 0;
-
-    //return 1 if at end
     if(start.name === 'end')
         return 1;
+
+        //dont revisit small caves
+    if(!start.isBig && haveVisited.has(start.name)){
+        if(start.name == revisit){
+            if(haveVisited.get(start.name) > 1)
+                return 0;
+        }
+        else
+            return 0;
+    }
     
     //mark this node as visited
     if(!start.isBig)
-        haveVisited.set(start.name, 1);
+        haveVisited.set(start.name, (haveVisited.get(start.name) ?? 0) + 1);
 
     //DFS to all edges
     let sum = 0;
     for(let i = 0; i < start.edges.length; i++){
-        sum += DFS(nodeMap.get(start.edges[i]), nodeMap, haveVisited);
+        sum += DFS(nodeMap.get(start.edges[i]), nodeMap, haveVisited, revisit);
     }
     
     //consider this node as unvisited for future, different paths
-    haveVisited.delete(start.name);
+    if(start.name == revisit && haveVisited.get(start.name) == 2)
+        haveVisited.set(start.name, 1);
+    else
+        haveVisited.delete(start.name);
+
     return sum;
 }
+
+//184895
+//218275
+//206763 (UNTRIED!)
+//179139
+//202163
+//213675
+//47188
+//144603
